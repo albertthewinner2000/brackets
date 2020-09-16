a7727e1b9d73b49c154491abe29fdd68bc515cf2
albertthewinner2000 <nalbert2012@ya.ru>
2020-09-16 07:17:20 +0300

    feat:add loop for same brackests

--- src/index.js
+++ src/index.js
@@ -1,73 +1,87 @@
 // module.exports =
 function check(str, bracketsConfig) {
     if (str.length % 2 === 0) {
+        //stage 0 (even length)
         const config = bracketsConfig.flat();
         let arr = str.split(""),
             currentBracket = str[0],
             pairBracket = config[config.indexOf(currentBracket) + 1],
             pairBracketIndex = 1;
-        // findPair = false;
-        console.log(arr, currentBracket, pairBracket);
+        let j = 0; //j- counter
+        console.log(arr);
         while (arr.length > 0) {
-            //currnent bracket validation
+            console.log(`new iteration ${j++}`);
             if (config.indexOf(currentBracket) % 2 !== 0) {
-                console.log("currnent bracket vaidation no passed");
+                //stage 1 (first bracket in array is not wrong)
                 return false;
             } else {
-                //pairBracket searching
-                // for (let i = 1; i < arr.length; i++) {
-                for (var i = 1; i < arr.length; i += 2) {
-                    console.log("pairBracket searching");
-                    console.log(pairBracket);
-                    if (
-                        // i % 2 !== 0 &&
-                        arr[i] === pairBracket &&
-                        arr[i + 1] !== pairBracket
-                    ) {
-                        pairBracketIndex = i;
-                        // findPair = true;
-                        console.log(
-                            `pair rigth, index: ${i}, pairIndex:${pairBracketIndex}`
-                        );
-                        console.log(
-                            arr,
-                            currentBracket,
-                            pairBracket,
-                            pairBracketIndex
-                        );
-                        break;
-                        // break?
-                    } else {
-                        // findPair = false;
-                        console.log("wrong pair, go next");
+                if (currentBracket === pairBracket) {
+                    //stage3.1 (brackets in pair is same)
+                    console.log(`go stage 3.1`);
+                    for (var i = 1; i < arr.length; i += 2) {
+                        if (arr[i] === pairBracket) {
+                            pairBracketIndex = i;
+                            console.log(`index ${i} value:${arr[i]} passed, pair Index: ${pairBracketIndex}`)
+                            break;
+                        } else {
+                            console.log(`index ${i} value:${arr[i]} did not passed, pair Index: ${pairBracketIndex}`)
+                        }
+                    }
+                } else {
+                    //stage3.2 (brackets in pair is same)
+                    console.log(`go stage 3.2`);
+                    for (var i = 1; i < arr.length; i += 2) {
+                        if (
+                            arr[i] === pairBracket &&
+                            arr[i + 1] !== pairBracket
+                            ) {
+                                console.log(`index ${i} value:${arr[i]} passed, pair Index: ${pairBracketIndex}`)
+                                pairBracketIndex = i;
+                                break;
+                            } else {
+                                console.log(`index ${i} value:${arr[i]} did not passed, pair Index: ${pairBracketIndex}`)
+                        }
                     }
                 }
-                console.log(`loop index: ${i}`);
                 if (--i >= arr.length && pairBracketIndex !== i) {
-                    console.log(`loop doesn't find a pair`);
                     return false;
                 }
-                //Change arr
+                //stage 4 (change array, first bracket and pair)
                 delete arr[pairBracketIndex];
                 arr = arr.filter(() => true);
                 arr.shift();
-                //currentBracket change
-                console.log("array change");
-                currentBracket = arr[0]; //currentBracket change
+                currentBracket = arr[0];
                 pairBracket = config[config.indexOf(currentBracket) + 1];
-                console.log(arr, currentBracket, pairBracket, pairBracketIndex);
+                console.log(
+                    `array:${arr}, bracket:${currentBracket}, pair:${pairBracket}, pair index:${pairBracketIndex} counter in loop:${i}`
+                );
             }
         }
+        //stage 5 (return true if all conditions passed)
         return true;
     }
     return false;
 }
 
-let result = check(
-    "||",[['|','|']]
-    
-);
+const config = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["|", "|"],
+    ["1", "2"],
+    ["3", "4"],
+    ["5", "6"],
+    ["7", "7"],
+    ["8", "8"],
+];
 
-// let result = 1%2 !==0;
+const result = check(
+    "{[{}]}",
+    config
+    // const result = check(
+    //     "([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())",
+    //     config
+);
+// const result = '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())'.length
 
 console.log(result);
